13fa19b93290c2fa5fd45242c6baf6257cbc29dd
pawaret <62021393@up.ac.th>
2022-01-20 02:06:52 +0700

    ๊Add dropdown

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { React, useEffect, useState } from "react";
+import { React, useEffect, useState, Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/solid";
 
 import logo from "./logo.svg";
 import arrowDown from "./assets/arrow-down.svg";
@@ -67,6 +69,42 @@ function App() {
     setHoverSwap(!hoverSwap);
   };
 
+  function EditInactiveIcon(props) {
+    return (
+      <svg
+        {...props}
+        viewBox="0 0 20 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M4 13V16H7L16 7L13 4L4 13Z"
+          fill="#EDE9FE"
+          stroke="#A78BFA"
+          strokeWidth="2"
+        />
+      </svg>
+    )
+  }
+
+  function EditActiveIcon(props) {
+    return (
+      <svg
+        {...props}
+        viewBox="0 0 20 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M4 13V16H7L16 7L13 4L4 13Z"
+          fill="#8B5CF6"
+          stroke="#C4B5FD"
+          strokeWidth="2"
+        />
+      </svg>
+    )
+  }
+
   return (
     <div className="h-screen w-screen flex items-center justify-center bg-background">
       <div className="h-11/12 w-11/12 flex justify-between">
@@ -129,7 +167,11 @@ function App() {
                 className="w-full p-3 rounded-xl shadow-md bg-hover"
               />
             </div>
-            <div className="h-5" onMouseEnter={toggleHover} onMouseLeave={toggleHover}>
+            <div
+              className="h-5"
+              onMouseEnter={toggleHover}
+              onMouseLeave={toggleHover}
+            >
               {hoverSwap ? (
                 <button>
                   <img src={swap} className="h-5 w-5 text-white" alt="logo" />
@@ -152,6 +194,58 @@ function App() {
                 className="w-full p-3 rounded-xl shadow-md bg-hover"
               />
             </div>
+            <Menu as="div" className="relative inline-block text-left">
+              <div>
+                <Menu.Button className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                  Options
+                  <ChevronDownIcon
+                    className="w-5 h-5 ml-2 -mr-1 text-violet-200 hover:text-violet-100"
+                    aria-hidden="true"
+                  />
+                </Menu.Button>
+              </div>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="absolute right-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <div className="px-1 py-1 ">
+                    <Menu.Item>
+                      {({ active }) => (
+                        <button
+                          className={`${
+                            active
+                              ? "bg-violet-500 text-white"
+                              : "text-gray-900"
+                          } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                        >
+                          {active ? (
+                            <EditActiveIcon
+                              className="w-5 h-5 mr-2"
+                              aria-hidden="true"
+                            />
+                          ) : (
+                            <EditInactiveIcon
+                              className="w-5 h-5 mr-2"
+                              aria-hidden="true"
+                            />
+                          )}
+                          Edit
+                        </button>
+                      )}
+                    </Menu.Item>
+                    
+                  </div>
+                  
+                  
+                </Menu.Items>
+              </Transition>
+            </Menu>
           </div>
         </div>
       </div>
