708f97fee957290f8052dfa0e17c95e467ec6db2
pawaret <62021393@up.ac.th>
2022-01-20 19:56:41 +0700

    Update select dropdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { React, useEffect, useState, Fragment } from "react";
-import { Menu, Transition } from "@headlessui/react";
+import { Listbox, Transition } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/solid";
+import { Scrollbars } from "react-custom-scrollbars";
 
 import logo from "./logo.svg";
 import arrowDown from "./assets/arrow-down.svg";
@@ -14,8 +15,22 @@ function App() {
   const [syncDate, setSyncDate] = useState();
   const [syncTime, setSyncTime] = useState();
   const [hoverSwap, setHoverSwap] = useState(false);
+  const [firstToken, setFirstToken] = useState("btc");
+  const [secondToken, setSecondToken] = useState("btc");
 
   useEffect(() => {
+    syncData();
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      syncData();
+      console.log("Logs every minute");
+    }, 60000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const syncData = () => {
     var currentdate = new Date();
     var date =
       currentdate.getDate() +
@@ -32,7 +47,9 @@ function App() {
     setSyncDate(date);
     setSyncTime(time);
 
-    const getAPI = fetch("https://jergun-bot.herokuapp.com/all")
+    fetch("https://jergun-bot.herokuapp.com/all", {
+      mode: "cors",
+    })
       .then((response) => {
         if (response.ok) {
           return response.json();
@@ -41,69 +58,16 @@ function App() {
       })
       .then((result) => {
         setData(result);
+        setFirstToken(result[0].id);
       })
       .catch((error) => {
         console.error("Error fetching data: ", error);
       });
-
-    const interval = setInterval(() => {
-      date =
-        currentdate.getDate() +
-        "/" +
-        (currentdate.getMonth() + 1) +
-        "/" +
-        currentdate.getFullYear();
-      time =
-        currentdate.getHours() +
-        ":" +
-        currentdate.getMinutes() +
-        ":" +
-        currentdate.getSeconds();
-      setSyncDate(date);
-      setSyncTime(time);
-      getAPI;
-    }, 300000);
-  }, []);
+  };
 
   const toggleHover = () => {
     setHoverSwap(!hoverSwap);
   };
-
-  function EditInactiveIcon(props) {
-    return (
-      <svg
-        {...props}
-        viewBox="0 0 20 20"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M4 13V16H7L16 7L13 4L4 13Z"
-          fill="#EDE9FE"
-          stroke="#A78BFA"
-          strokeWidth="2"
-        />
-      </svg>
-    )
-  }
-
-  function EditActiveIcon(props) {
-    return (
-      <svg
-        {...props}
-        viewBox="0 0 20 20"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M4 13V16H7L16 7L13 4L4 13Z"
-          fill="#8B5CF6"
-          stroke="#C4B5FD"
-          strokeWidth="2"
-        />
-      </svg>
-    )
-  }
 
   return (
     <div className="h-screen w-screen flex items-center justify-center bg-background">
@@ -116,8 +80,11 @@ function App() {
             <p className="text-primary">{syncTime}</p>
           </div>
           <div className="flex flex-wrap">
-            {data?.map((item) => (
-              <div className="h-fit w-72 p-5 m-5 rounded-2xl space-y-1 shadow-lg text-white bg-input">
+            {data?.map((item, i) => (
+              <div
+                key={i}
+                className="h-fit w-72 p-5 m-5 rounded-2xl space-y-1 shadow-lg text-white bg-input"
+              >
                 <div className="flex items-center space-x-3">
                   <img
                     key={item.id}
@@ -159,8 +126,68 @@ function App() {
           <div className="w-full p-5 flex flex-col items-center space-y-5">
             <div className="w-full space-y-3">
               <div className="flex items-center space-x-1 font-bold text-xl">
-                <img src={logo} className="h-10 w-10" alt="logo" />
-                <p>sad</p>
+                <Listbox value={firstToken} onChange={setFirstToken}>
+                  <div className="relative inline-block text-left">
+                    <div>
+                      <Listbox.Button className="inline-flex justify-center items-center space-x-3 w-full font-bold text-xl text-white rounded-md">
+                        <img src={require(`./assets/${firstToken}.svg`)} className="h-6 w-6" alt="logo" />
+                        <p className="uppercase">{firstToken}</p>
+                        <ChevronDownIcon
+                          className="w-5 h-5 ml-2 -mr-1 text-violet-200 hover:text-violet-100"
+                          aria-hidden="true"
+                        />
+                      </Listbox.Button>
+                    </div>
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <Listbox.Options className="absolute left-0 w-56 mt-3 origin-top-right bg-sub-text rounded-xl shadow-lg">
+                        <div className="h-72 px-1 py-1 ">
+                          <Scrollbars>
+                            {data?.map((item, i) => (
+                              <Listbox.Option key={i} value={item.id} >
+                                {({ active }) => (
+                                  <button
+                                    className={`${
+                                      active
+                                        ? "bg-background text-white disabled"
+                                        : "text-black"
+                                    } group flex rounded-xl items-center space-x-5 w-full px-5 py-2 font-bold text-lg`}
+                                    
+                                  >
+                                    <img
+                                      src={require(`./assets/${item.id}.svg`)}
+                                      className="h-6 w-6"
+                                      alt="logo"
+                                    />
+                                    <div className="flex flex-col items-start">
+                                      <p>{item.symbol}</p>
+                                      <p
+                                        className={
+                                          active
+                                            ? "text-text"
+                                            : "text-base text-input"
+                                        }
+                                      >
+                                        {item.name}
+                                      </p>
+                                    </div>
+                                  </button>
+                                )}
+                              </Listbox.Option>
+                            ))}
+                          </Scrollbars>
+                        </div>
+                      </Listbox.Options>
+                    </Transition>
+                  </div>
+                </Listbox>
               </div>
               <input
                 type="number"
@@ -168,7 +195,7 @@ function App() {
               />
             </div>
             <div
-              className="h-5"
+              className="h-10 w-10 flex justify-center items-center rounded-lg shadow-md bg-hover"
               onMouseEnter={toggleHover}
               onMouseLeave={toggleHover}
             >
@@ -186,66 +213,74 @@ function App() {
             </div>
             <div className="w-full space-y-3">
               <div className="flex items-center space-x-1 font-bold text-xl">
-                <img src={logo} className="h-10 w-10" alt="logo" />
-                <p>asd</p>
+              <Listbox value={secondToken} onChange={setSecondToken}>
+                  <div className="relative inline-block text-left">
+                    <div>
+                      <Listbox.Button className="inline-flex justify-center items-center space-x-3 w-full font-bold text-xl text-white rounded-md">
+                        <img src={require(`./assets/${secondToken}.svg`)} className="h-6 w-6" alt="logo" />
+                        <p className="uppercase">{secondToken}</p>
+                        <ChevronDownIcon
+                          className="w-5 h-5 ml-2 -mr-1 text-violet-200 hover:text-violet-100"
+                          aria-hidden="true"
+                        />
+                      </Listbox.Button>
+                    </div>
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <Listbox.Options className="absolute left-0 w-56 mt-3 origin-top-right bg-sub-text rounded-xl shadow-lg">
+                        <div className="h-72 px-1 py-1 ">
+                          <Scrollbars>
+                            {data?.map((item, i) => (
+                              <Listbox.Option key={i} value={item.id} >
+                                {({ active }) => (
+                                  <button
+                                    className={`${
+                                      active
+                                        ? "bg-background text-white disabled"
+                                        : "text-black"
+                                    } group flex rounded-xl items-center space-x-5 w-full px-5 py-2 font-bold text-lg`}
+                                    
+                                  >
+                                    <img
+                                      src={require(`./assets/${item.id}.svg`)}
+                                      className="h-6 w-6"
+                                      alt="logo"
+                                    />
+                                    <div className="flex flex-col items-start">
+                                      <p>{item.symbol}</p>
+                                      <p
+                                        className={
+                                          active
+                                            ? "text-text"
+                                            : "text-base text-input"
+                                        }
+                                      >
+                                        {item.name}
+                                      </p>
+                                    </div>
+                                  </button>
+                                )}
+                              </Listbox.Option>
+                            ))}
+                          </Scrollbars>
+                        </div>
+                      </Listbox.Options>
+                    </Transition>
+                  </div>
+                </Listbox>
               </div>
               <input
                 type="number"
                 className="w-full p-3 rounded-xl shadow-md bg-hover"
               />
             </div>
-            <Menu as="div" className="relative inline-block text-left">
-              <div>
-                <Menu.Button className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-                  Options
-                  <ChevronDownIcon
-                    className="w-5 h-5 ml-2 -mr-1 text-violet-200 hover:text-violet-100"
-                    aria-hidden="true"
-                  />
-                </Menu.Button>
-              </div>
-              <Transition
-                as={Fragment}
-                enter="transition ease-out duration-100"
-                enterFrom="transform opacity-0 scale-95"
-                enterTo="transform opacity-100 scale-100"
-                leave="transition ease-in duration-75"
-                leaveFrom="transform opacity-100 scale-100"
-                leaveTo="transform opacity-0 scale-95"
-              >
-                <Menu.Items className="absolute right-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                  <div className="px-1 py-1 ">
-                    <Menu.Item>
-                      {({ active }) => (
-                        <button
-                          className={`${
-                            active
-                              ? "bg-violet-500 text-white"
-                              : "text-gray-900"
-                          } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
-                        >
-                          {active ? (
-                            <EditActiveIcon
-                              className="w-5 h-5 mr-2"
-                              aria-hidden="true"
-                            />
-                          ) : (
-                            <EditInactiveIcon
-                              className="w-5 h-5 mr-2"
-                              aria-hidden="true"
-                            />
-                          )}
-                          Edit
-                        </button>
-                      )}
-                    </Menu.Item>
-                    
-                  </div>
-                  
-                  
-                </Menu.Items>
-              </Transition>
-            </Menu>
           </div>
         </div>
       </div>
